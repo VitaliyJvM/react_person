b91a674a664bc123c12ea5f5ed97e5c7791ae5c5
Vitalii Martynov <vitalii.martynov@sixt.com>
2025-11-05 12:14:27 -0400

    Considered comments.

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 
 const MarriedPartner = ({ sex, partnerName }) => (
   <p className="Person__partner">
-    {`${partnerName} is my ${
-      sex === 'm'
-        ? 'wife'
-        : 'husband'
-    }`}
+    {`${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`}
   </p>
 );
 
